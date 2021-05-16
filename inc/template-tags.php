0e7910f8612e5d9767e68299c5bd67448c6b7d1e
Nikola <nikola.tomasovic@medieinstitutet.se>
2021-05-16 19:09:41 +0200

    added custom post type blog and taxonomy tags and categories

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -238,7 +238,7 @@ if (!function_exists('bootscore_recipe_gallery')) {
 				<ul class="slides">
 					<?php foreach ($gallery as $image): ?>
 						<li>
-							<img src="<?php echo $image['url']; ?>">
+							<img src="<?php echo $image['url'], 'thumbnail'; ?>">
 						</li>
 					<?php endforeach; ?>
 				</ul>
