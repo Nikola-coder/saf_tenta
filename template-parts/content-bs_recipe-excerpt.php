e34ca1c303d1f90a0f76e197dc5bd6384a291119
Nikola <nikola.tomasovic@medieinstitutet.se>
2021-05-19 16:41:48 +0200

    added anchor on thumbnails in bs-recipes archives

--- template-parts/content-bs_recipe-excerpt.php
+++ template-parts/content-bs_recipe-excerpt.php
@@ -3,57 +3,65 @@
                     <!-- Title & Description -->
                     <header class="page-header mb-4">
                         <h1><?php the_archive_title(); ?></h1>
-                        <?php the_archive_description( '<div class="archive-description">', '</div>' ); ?>
+                        <?php the_archive_description('<div class="archive-description">', '</div>'); ?>
                     </header>
 
                     <!-- Grid Layout -->
-                    <?php if (have_posts() ) : ?>
-                    <?php while (have_posts() ) : the_post(); ?>
-                    <div class="card horizontal mb-4">
-                        <div class="row">
-                            <!-- Featured Image-->
-                            <?php if (has_post_thumbnail() )
-							echo '<div class="card-img-left-md col-lg-5">' . get_the_post_thumbnail(null, 'medium') . '</div>';
-							?>
-                            <div class="col">
-                                <div class="card-body">
-                                    
-                                    <!-- recipe score -->
-                                    <?php bootscore_recipe_score_badge(); ?>
-                                    
-                                    <!-- Title -->
-                                    <h2 class="blog-post-title">
+                    <?php if (have_posts()) : ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <div class="card horizontal mb-4">
+                                <div class="row">
+
+                                    <!-- Featured Image-->
+                                    <div class="col-md-3">
                                         <a href="<?php the_permalink(); ?>">
-                                            <?php the_title(); ?>
+                                            <?php the_post_thumbnail('featured-image-thumb', ['class' => 'img-fluid']); ?>
+
+                                            <?php if (has_post_thumbnail())
+
+                                            ?>
                                         </a>
-                                    </h2>
-
-                                    <!--  category tag-->
-                                    <?php bootscore_recipe_category_badge(); ?>
-
-                                    <!-- Meta -->
-                                    <?php if ( 'post' === get_post_type() ) : ?>
-                                    <small class="text-muted mb-2">
-                                        <?php
-									bootscore_date();
-									bootscore_author();
-									bootscore_comments();
-									bootscore_edit();
-									?>
-                                    </small>
-                                    <?php endif; ?>
-                                    <!-- Excerpt & Read more -->
-                                    <div class="card-text mt-auto">
-                                        <?php the_excerpt(); ?> <a class="read-more btn btn-info" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'recipes'); ?></a>
                                     </div>
-                                    <!--  recipe tag-->
-                                    <?php bootscore_recipe_tag_badge(); ?>
-                                    <?php bootscore_tags(); ?>
+
+                                    <div class="col">
+                                        <div class="card-body">
+
+                                            <!-- recipe score -->
+                                            <?php bootscore_recipe_score_badge(); ?>
+
+                                            <!-- Title -->
+                                            <h2 class="blog-post-title">
+                                                <a href="<?php the_permalink(); ?>">
+                                                    <?php the_title(); ?>
+                                                </a>
+                                            </h2>
+
+                                            <!--  category tag-->
+                                            <?php bootscore_recipe_category_badge(); ?>
+
+                                            <!-- Meta -->
+                                            <?php if ('post' === get_post_type()) : ?>
+                                                <small class="text-muted mb-2">
+                                                    <?php
+                                                    bootscore_date();
+                                                    bootscore_author();
+                                                    bootscore_comments();
+                                                    bootscore_edit();
+                                                    ?>
+                                                </small>
+                                            <?php endif; ?>
+                                            <!-- Excerpt & Read more -->
+                                            <div class="card-text mt-auto">
+                                                <?php the_excerpt(); ?> <a class="read-more btn btn-info" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'recipes'); ?></a>
+                                            </div>
+                                            <!--  recipe tag-->
+                                            <?php bootscore_recipe_tag_badge(); ?>
+                                            <?php bootscore_tags(); ?>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <?php endwhile; ?>
+                        <?php endwhile; ?>
                     <?php endif; ?>
 
                     <!-- Pagination -->
